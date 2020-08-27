aff3bbea7c9500b99dcad91c40b6757fcf0ba419
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-08-27 08:07:04 +0300

    code from lesson-3 (2020-08-25 Tabelskiy)

---
+++ cache_decor.py
@@ -0,0 +1,39 @@
+import time
+from collections import OrderedDict
+
+
+def cachable(param):
+    CACHE = OrderedDict()
+
+    def _cachable(old_function):
+
+        def new_function(*args, **kwargs):
+            print(len(CACHE))
+            key = (old_function.__name__, str(args), str(kwargs))
+            result = CACHE.get(key)
+            if result is not None:
+                return result
+            else:
+                result = old_function(*args, **kwargs)
+                CACHE[key] = result
+                if len(CACHE) > param:
+                    CACHE.popitem(last=False)
+                return result
+
+        return new_function
+
+    return _cachable
+
+
+@cachable(4)
+def concat(str_1, str_2):
+    time.sleep(2)
+    return f'{str_1}{str_2}'
+
+
+print(concat('abc', 'erd'))
+print(concat('abc', 'erd1'))
+print(concat('abc', 'erd2'))
+print(concat('abc', 'erd3'))
+print(concat('abc', 'erd4'))
+print(concat('abc', 'erd5'))
---
+++ size_decor.py
@@ -0,0 +1,64 @@
+from pympler import asizeof
+
+
+def size_decor(old_function):
+    calls = 0
+    total_size = 0
+
+    def new_function(*args, **kwargs):
+        nonlocal calls, total_size
+        calls += 1
+
+        result = old_function(*args, **kwargs)
+        size_result = asizeof.asizeof(result)
+        total_size += size_result
+        medium_size = total_size // calls
+
+        print(f'Вызов:{old_function.__name__}\n'
+              f'Размер: {size_result}\n'
+              f'В среднем: {medium_size}')
+        return result
+
+    return new_function
+
+
+def parametrized_size_decor(min_size):
+    calls = 0
+    total_size = 0
+
+    def size_decor(old_function):
+        def new_function(*args, **kwargs):
+            nonlocal calls, total_size
+            calls += 1
+
+            result = old_function(*args, **kwargs)
+            size_result = asizeof.asizeof(result)
+            total_size += size_result
+            medium_size = total_size // calls
+
+            if size_result >= min_size:
+                print(f'Вызов:{old_function.__name__}\n'
+                      f'Размер: {size_result}\n'
+                      f'В среднем: {medium_size}')
+
+            return result
+
+        return new_function
+
+    return size_decor
+
+
+@size_decor
+def foo():
+    return [i for i in range(10000)]
+
+
+@parametrized_size_decor(50000)
+def foo2(n):
+    return [i for i in range(n)]
+
+
+if __name__ == '__main__':
+    foo()
+    foo2(10)
+    foo2(100000)
